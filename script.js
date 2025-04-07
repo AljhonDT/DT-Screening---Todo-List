b84bd061a062331a7de0953e4788ab6dad87e711
AljhonDT <aljhon.delatorre17@gmail.com>
2025-04-07 14:25:17 +0800

    function for adding task

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 const taskInput = document.getElementById("task-input");
 const addTaskBtn = document.getElementById("add-task-btn");
 const taskList = document.getElementById("task-list");
+
+const addTask = () => {
+    const taskText = taskInput.value.trim();
+    if (taskText !== "") {
+            const addedTask = document.createElement("li");
+            addedTask.textContent = taskText;
+            taskList.appendChild(addedTask);
+            taskInput.value = "";
+    }
+}
+addTaskBtn.addEventListener("click", addTask);
